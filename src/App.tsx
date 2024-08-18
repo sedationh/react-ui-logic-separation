8761285b631f50da835698dbabf35d2fe7fe646d
SedationH <sedationh@gmail.com>
2024-08-18 20:15:35 +0800

    1 state method

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,59 @@
+import { useReducer, useRef } from "react";
+
+type A = ReturnType<Core["getProps"]>;
+
+class Core {
+  updater: () => void;
+
+  constructor(updater) {
+    this.updater = updater;
+  }
+
+  state = {
+    title: "Hello World",
+  };
+
+  updateTitle = (newTitle = "newTitle") => {
+    this.state.title = newTitle;
+    this.updater();
+  };
+
+  getProps = () => {
+    return {
+      state: this.state,
+      methods: {
+        updateTitle: this.updateTitle,
+      },
+    };
+  };
+}
+
+function UI({ state, methods }: A) {
+  return (
+    <div>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+      <button onClick={() => methods.updateTitle(state.title + "1")}>
+        Update Title
+      </button>
+    </div>
+  );
+}
+
 function App() {
-  return <div>This is App</div>;
+  const coreRef = useRef<Core>(null);
+
+  const [, forceUpdate] = useReducer((c) => c + 1, 0);
+
+  if (coreRef.current === null) {
+    coreRef.current = new Core(forceUpdate);
+  }
+
+  return (
+    <div>
+      <h1>UI</h1>
+      <UI {...coreRef.current.getProps()} />
+    </div>
+  );
 }
 
 export default App;
